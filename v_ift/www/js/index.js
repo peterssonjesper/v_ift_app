35aaba65ab5513e585ef30776167172205be5aaf
Christoffer Åhrling <christoffer@nilpath.se>
2014-11-23 10:32:39 +0100

    added cordova plugins

--- v_ift/www/js/index.js
+++ v_ift/www/js/index.js
@@ -34,6 +34,23 @@ var app = {
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
+        navigator.geolocation.getCurrentPosition(onSuccess, onError);
+        
+        function onSuccess(position) {
+          alert('Latitude: '          + position.coords.latitude          + '\n' +
+          'Longitude: '         + position.coords.longitude         + '\n' +
+          'Altitude: '          + position.coords.altitude          + '\n' +
+          'Accuracy: '          + position.coords.accuracy          + '\n' +
+          'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+          'Heading: '           + position.coords.heading           + '\n' +
+          'Speed: '             + position.coords.speed             + '\n' +
+          'Timestamp: '         + position.timestamp                + '\n');
+        }
+        
+        function onError(error) {
+          alert('code: '    + error.code    + '\n' +
+          'message: ' + error.message + '\n');
+        }
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
